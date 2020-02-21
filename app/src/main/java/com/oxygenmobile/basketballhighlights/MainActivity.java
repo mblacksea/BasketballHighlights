6e69066dfee2644270ab0867d0d29511be58ebef
mustafa.karadeniz <mustafa.karadeniz@etiya.com>
2020-02-21 16:57:43 +0300

    Some fix.

--- app/src/main/java/com/oxygenmobile/basketballhighlights/MainActivity.java
+++ app/src/main/java/com/oxygenmobile/basketballhighlights/MainActivity.java
@@ -2,13 +2,13 @@ package com.oxygenmobile.basketballhighlights;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 
 import com.onurkaganaldemir.ktoastlib.KToast;
 import com.oxygenmobile.basketballhighlights.activity.HighlightsActivity;
 import com.oxygenmobile.basketballhighlights.activity.Top10PlaysActivity;
+import com.oxygenmobile.basketballhighlights.utils.NavigateUtils;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -17,15 +17,13 @@ import butterknife.OnClick;
 public class MainActivity extends AppCompatActivity {
     @OnClick(R.id.highlightsPlays)
     void onHighlightsPlays() {
-        Intent intent = new Intent(getApplicationContext(), HighlightsActivity.class);
-        startActivity(intent);
+        NavigateUtils.navigateToActivityWithoutValue(getApplicationContext(), HighlightsActivity.class);
         KToast.successToast(MainActivity.this, "Highlights on click", Gravity.BOTTOM, KToast.LENGTH_AUTO);
     }
 
     @OnClick(R.id.topTenPlays)
     void onTopTenPlays() {
-        Intent intent = new Intent(getApplicationContext(), Top10PlaysActivity.class);
-        startActivity(intent);
+        NavigateUtils.navigateToActivityWithoutValue(getApplicationContext(), Top10PlaysActivity.class);
         KToast.successToast(MainActivity.this, "Top 10 Plays on click", Gravity.BOTTOM, KToast.LENGTH_AUTO);
     }
 
