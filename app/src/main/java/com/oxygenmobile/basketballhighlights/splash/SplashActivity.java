4816970473148c17047c44837a3035e79c81a8f0
TCMUKARADENIZ <mustafa.karadeniz@turkcell.com.tr>
2020-02-25 09:36:11 +0300

    color change.

--- app/src/main/java/com/oxygenmobile/basketballhighlights/splash/SplashActivity.java
+++ app/src/main/java/com/oxygenmobile/basketballhighlights/splash/SplashActivity.java
@@ -42,7 +42,7 @@ public class SplashActivity extends AwesomeSplash {
     @Override
     public void initSplash(ConfigSplash configSplash) {
         //Customize Circular Reveal
-        configSplash.setBackgroundColor(R.color.colorAccent); //any color you want form colors.xml
+        configSplash.setBackgroundColor(R.color.splashBackground); //any color you want form colors.xml
         configSplash.setAnimCircularRevealDuration(2000); //int ms
         configSplash.setRevealFlagX(Flags.REVEAL_RIGHT);  //or Flags.REVEAL_LEFT
         configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM); //or Flags.REVEAL_TOP
@@ -52,18 +52,18 @@ public class SplashActivity extends AwesomeSplash {
         //Customize Logo
         configSplash.setLogoSplash(R.mipmap.ic_launcher); //or any other drawable
         configSplash.setAnimLogoSplashDuration(1000); //int ms
-        configSplash.setAnimLogoSplashTechnique(Techniques.Bounce); //choose one form Techniques (ref: https://github.com/daimajia/AndroidViewAnimations)
+        configSplash.setAnimLogoSplashTechnique(Techniques.FadeIn); //choose one form Techniques (ref: https://github.com/daimajia/AndroidViewAnimations)
 
 
         //Customize Path
         //configSplash.setPathSplash(Constants.DROID_LOGO); //set path String
-        configSplash.setOriginalHeight(400); //in relation to your svg (path) resource
+        /*configSplash.setOriginalHeight(400); //in relation to your svg (path) resource
         configSplash.setOriginalWidth(400); //in relation to your svg (path) resource
         configSplash.setAnimPathStrokeDrawingDuration(1000);
         configSplash.setPathSplashStrokeSize(3); //I advise value be <5
         configSplash.setPathSplashStrokeColor(R.color.colorAccent); //any color you want form colors.xml
         configSplash.setAnimPathFillingDuration(3000);
-        configSplash.setPathSplashFillColor(R.color.colorAccent); //path object filling color
+        configSplash.setPathSplashFillColor(R.color.colorAccent);*/ //path object filling color
 
 
         //Customize Title
